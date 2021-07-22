9cb3e8561d64cb9207e62f3a2ee566b0d23cb293
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-07-22 21:19:31 +0200

    Prettier formatting

--- exercises/exercise-2/exercise-2.js
+++ exercises/exercise-2/exercise-2.js
@@ -1,88 +1,84 @@
 let hogwarts = [
-  {
-    firstName: "Harry",
-    lastName: "Potter",
-    house: "Gryffindor",
-    pet: "Hedwig",
-    occupation: "Student",
-  },
-  {
-    firstName: "Ron",
-    lastName: "Weasley",
-    house: "Gryffindor",
-    pet: "Scabbers",
-    occupation: "Student",
-  },
-  {
-    firstName: "Hermione",
-    lastName: "Granger",
-    house: "Gryffindor",
-    pet: "Crookshanks",
-    occupation: "Student",
-  },
-  {
-    firstName: "Draco",
-    lastName: "Malfoy",
-    house: "Slytherin",
-    pet: null,
-    occupation: "Student",
-  },
-  {
-    firstName: "Cedric",
-    lastName: "Diggory",
-    house: "HufflePuff",
-    pet: null,
-    occupation: "Student",
-  },
-  {
-    firstName: "Severus",
-    lastName: "Snape",
-    house: "Slytherin",
-    pet: null,
-    occupation: "Teacher",
-  },
-  {
-    firstName: "Filius",
-    lastName: "Flitwick",
-    house: "Ravenclaw",
-    pet: null,
-    occupation: "Teacher",
-  },
-  {
-    firstName: "Pomona",
-    lastName: "Sprout",
-    house: "Hufflepuff",
-    pet: null,
-    occupation: "Teacher",
-  },
-  {
-    firstName: "Minerva",
-    lastName: "McGonagall",
-    house: "Gryffindor",
-    pet: null,
-    occupation: "Teacher",
-  },
-  {
-    firstName: "Albus",
-    lastName: "Dumbledore",
-    house: "Gryffindor",
-    pet: "Fawkes",
-    occupation: "Teacher",
-  },
+	{
+		firstName: "Harry",
+		lastName: "Potter",
+		house: "Gryffindor",
+		pet: "Hedwig",
+		occupation: "Student",
+	},
+	{
+		firstName: "Ron",
+		lastName: "Weasley",
+		house: "Gryffindor",
+		pet: "Scabbers",
+		occupation: "Student",
+	},
+	{
+		firstName: "Hermione",
+		lastName: "Granger",
+		house: "Gryffindor",
+		pet: "Crookshanks",
+		occupation: "Student",
+	},
+	{
+		firstName: "Draco",
+		lastName: "Malfoy",
+		house: "Slytherin",
+		pet: null,
+		occupation: "Student",
+	},
+	{
+		firstName: "Cedric",
+		lastName: "Diggory",
+		house: "HufflePuff",
+		pet: null,
+		occupation: "Student",
+	},
+	{
+		firstName: "Severus",
+		lastName: "Snape",
+		house: "Slytherin",
+		pet: null,
+		occupation: "Teacher",
+	},
+	{
+		firstName: "Filius",
+		lastName: "Flitwick",
+		house: "Ravenclaw",
+		pet: null,
+		occupation: "Teacher",
+	},
+	{
+		firstName: "Pomona",
+		lastName: "Sprout",
+		house: "Hufflepuff",
+		pet: null,
+		occupation: "Teacher",
+	},
+	{
+		firstName: "Minerva",
+		lastName: "McGonagall",
+		house: "Gryffindor",
+		pet: null,
+		occupation: "Teacher",
+	},
+	{
+		firstName: "Albus",
+		lastName: "Dumbledore",
+		house: "Gryffindor",
+		pet: "Fawkes",
+		occupation: "Teacher",
+	},
 ];
 
 //Display Name and Surname function
-function displayCharacterName (ObjArr){
-  for (let character of ObjArr){
-
-    if(character.house === "Gryffindor"){
-
-    let {firstName, lastName} = character;
-    console.log(firstName, lastName);
-
-    }
-
-  }
+function displayCharacterName(ObjArr) {
+	for (let character of ObjArr) {
+		if (character.house === "Gryffindor") {
+			let { firstName, lastName } = character;
+			console.log(firstName, lastName);
+		}
+	}
 }
 /* Study session solution
 hogwarts.filter(({house}) => house === "Gryffindor")
@@ -90,13 +86,13 @@ hogwarts.filter(({house}) => house === "Gryffindor")
 */
 
 // FUnction to display teacher's who have pets
-function displayTeachersWithPets(objArr){
-  for (let character of objArr){
-    if(character.occupation === "Teacher" && character.pet){
-      let {firstName, lastName} = character;
-      console.log(firstName, lastName);
-    }
-  }
+function displayTeachersWithPets(objArr) {
+	for (let character of objArr) {
+		if (character.occupation === "Teacher" && character.pet) {
+			let { firstName, lastName } = character;
+			console.log(firstName, lastName);
+		}
+	}
 }
 /*
 Study session solution
--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -1,24 +1,29 @@
 let order = [
-  { itemName: "Hot cakes", quantity: 1, unitPrice: 2.29 },
-  { itemName: "Apple Pie", quantity: 2, unitPrice: 1.39 },
-  { itemName: "Egg McMuffin", quantity: 1, unitPrice: 2.8 },
-  { itemName: "Sausage McMuffin", quantity: 1, unitPrice: 3.0 },
-  { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
-  { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
+	{ itemName: "Hot cakes", quantity: 1, unitPrice: 2.29 },
+	{ itemName: "Apple Pie", quantity: 2, unitPrice: 1.39 },
+	{ itemName: "Egg McMuffin", quantity: 1, unitPrice: 2.8 },
+	{ itemName: "Sausage McMuffin", quantity: 1, unitPrice: 3.0 },
+	{ itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
+	{ itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
 
-
-function printReceipt (objectArr){
-  let total = [];
-  console.log("QTY ITEM            TOTAL");
-  for (let item of objectArr){
-    let {itemName, quantity, unitPrice} = item;
-    console.log(`${quantity}   ${itemName}     ${(quantity *unitPrice).toFixed(2)}`);
-    total.push(unitPrice * quantity);
-  }
-  console.log("Total:" + " "+ total.reduce( (acc, curr, currInd) =>{ 
-    return acc + curr;
-  }));
+function printReceipt(objectArr) {
+	let total = [];
+	console.log("QTY ITEM            TOTAL");
+	for (let item of objectArr) {
+		let { itemName, quantity, unitPrice } = item;
+		console.log(
+			`${quantity}   ${itemName}     ${(quantity * unitPrice).toFixed(2)}`
+		);
+		total.push(unitPrice * quantity);
+	}
+	console.log(
+		"Total:" +
+			" " +
+			total.reduce((acc, curr, currInd) => {
+				return acc + curr;
+			})
+	);
 }
 
 printReceipt(order);
